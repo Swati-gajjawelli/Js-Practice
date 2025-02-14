371c9509b3338579f77a28f859723e90cb8caa86
Swati-gajjawelli <98774607+Swati-gajjawelli@users.noreply.github.com>
2025-02-14 09:37:40 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version of version of JS.
+
+//alert(3+3); // We are using node js , not browser
+
+console.log(3+
+    3);
+// code readability should be high. Means code should be readable.
+
+let name = "Swati";    
+let age = 21;
+let isLoggedIn = false;
+
+// number 
+// bigint 
+// String
+// boolean
+// null => standalone value / representation of empty variable  
+// undefined => value not assigned is undefined.
+// symbol => used for uniqueness 
+
+// object 
+console.log(typeof isLoggedIn);
+
+console.log(typeof undefined);// undefined
+console.log(typeof null); // object
+
+
+
